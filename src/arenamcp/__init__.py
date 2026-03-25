be1e0b5724e2456af3a5355d0c9cd5e2cfb55c97
josharmour <1240306+josharmour@users.noreply.github.com>
2026-03-24 22:25:13 -0700

    v1.0.3: fix SQLite MISUSE error in card database

--- src/arenamcp/__init__.py
+++ src/arenamcp/__init__.py
@@ -61,7 +61,7 @@ except ImportError:
 
 from arenamcp.gamestate import save_match_state, load_match_state, mark_match_ended
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 def create_log_pipeline(
