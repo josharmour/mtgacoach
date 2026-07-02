ecbb9a7cc8cecb46ab4910f7521624850c3d1a6e
josharmour <1240306+josharmour@users.noreply.github.com>
2026-07-02 05:16:15 -0700

    Bump bridge plugin to v0.6.3 (two-phase target commit)

--- bepinex-plugin/MtgaCoachBridge/Plugin.cs
+++ bepinex-plugin/MtgaCoachBridge/Plugin.cs
@@ -4635,6 +4635,6 @@ namespace MtgaCoachBridge
     {
         public const string GUID = "com.mtgacoach.grebridge";
         public const string Name = "MtgaCoach GRE Bridge";
-        public const string Version = "0.6.2";
+        public const string Version = "0.6.3";
     }
 }
